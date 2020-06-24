7d9d4577f40fbd7617e4a4b263a799a6be26ee6c
Scott Ricketts <SJR@iMac.local>
2020-06-24 15:17:01 -0500

    added soem psuedocode

--- pybank.py/main.py
+++ pybank.py/main.py
@@ -1,12 +1,18 @@
 import os
 import csv
 
-csvpath = os.path.join('..' , 'Resources', 'budget_data.csv')
+csvpath = os.path.join("..", "Resources", "budget_data.csv")
 with open(csvpath) as csvfile:
-
     csvreader = csv.reader(csvfile, delimiter=',')
 
 
 
 
 # total_months =
+# use row[]
+# total number of months
+    # .count() -1
+# variable for first month
+    # row [1]
+# variable for last month
+    # row [8x]
