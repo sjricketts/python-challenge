c380d01073c296bbaa8255aa343e1a1dad978d90
Scott Ricketts <SJR@iMac.local>
2020-06-24 15:29:59 -0500

    fixed import csv issue

--- pybank.py/main.py
+++ pybank.py/main.py
@@ -1,9 +1,11 @@
 import os
 import csv
 
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+csvpath = os.path.join("Resources", "budget_data.csv")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+
+
 
 
 
