6379186e42b2a006c51ee18ce739084bdaf5ffe3
Scott Ricketts <SJR@iMac.local>
2020-06-26 13:47:02 -0500

    calculated total votes

--- PyPoll.py/main.py
+++ PyPoll.py/main.py
@@ -5,6 +5,23 @@ import csv
 # paths for csv file
 csvpath = os.path.join("Resources", "election_data.csv")
 
+# set variables
+total_votes = 0
+
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
+
+    for row in csvreader:
+        total_votes = total_votes + 1
+
+
+# Printed Analysis
+print("Election Results")
+print("--------------------------")
+print(f"Total Votes: {total_votes}")
+print("--------------------------")
+print(f"Khan: ( )")    # look up how to include percentages
+print(f"Correy: ( )")
+print(f"Li:  ( )")
+print(f"O'Tooley: ( )")
