34d6fa06e4f35d664f275f05553938d83c9b9abd
Scott Ricketts <SJR@iMac.local>
2020-06-26 16:43:24 -0500

    percentage of votes; need to convert format

--- PyPoll.py/main.py
+++ PyPoll.py/main.py
@@ -7,12 +7,12 @@ csvpath = os.path.join("Resources", "election_data.csv")
 
 # set variables
 total_votes = 0
-received_votes = []
 candidate = []
 kahn_votes = 0
 correy_votes = 0
 li_votes = 0
 otooley_votes = 0
+winner = []
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -26,26 +26,41 @@ with open(csvpath) as csvfile:
         # create iterable list for individual candidate's vote count
         candidate.append(row[2])
     
-        # store number of votes for each candidate in respctive variable
-        for i in candidate:
-            if candidate[i] == "Khan":
-                kahn_votes = kahn_votes + 1
-            elif candidate[i] == "Correy":
-                correy_votes = correy_votes + 1
-            elif candidate[i] == "Li":
-                li_votes = li_votes + 1
-            else:
-                otooley_votes = otooley_votes + 1
-        
+    # store number of votes for each candidate in respctive variable
+    for i in candidate:
+        if i == "Khan":
+            kahn_votes = kahn_votes + 1
+        elif i == "Correy":
+            correy_votes = correy_votes + 1
+        elif i == "Li":
+            li_votes = li_votes + 1
+        else:
+            otooley_votes = otooley_votes + 1
+
+# determine winner
+if kahn_votes >
+
+
+# figure percentage of votes for each candidate       
+kahn_percent = kahn_votes/total_votes
+correy_percent = correy_votes/total_votes
+li_percent = li_votes/total_votes
+otooley_percent = otooley_votes/total_votes
+
+# format to a percent
+kahn_format = "{:.2 %}".format(kahn_percent)
 
 # Printed Analysis
 print("Election Results")
 print("--------------------------")
 print(f"Total Votes: {total_votes}")
 print("--------------------------")
-print(f"Khan: ( )")    # look up how to include percentages
-print(f"Correy: ( )")
-print(f"Li:  ( )")
-print(f"O'Tooley: ( )")
+print(f"Khan: {kahn_format} ({kahn_votes})")    # look up how to include percentages
+print(f"Correy: {correy_percent} ({correy_votes})")
+print(f"Li: {li_percent} ({li_votes})")
+print(f"O'Tooley: {otooley_percent} ({otooley_votes})")
+print("--------------------------")
+print("Winner: ")
+print("--------------------------")
 
 # export results as text file
