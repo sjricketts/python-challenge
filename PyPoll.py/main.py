3c754030ee089798efd5523705988b999d47b16b
Scott Ricketts <SJR@iMac.local>
2020-06-26 14:10:44 -0500

    created loop for candidates vote count

--- PyPoll.py/main.py
+++ PyPoll.py/main.py
@@ -7,14 +7,36 @@ csvpath = os.path.join("Resources", "election_data.csv")
 
 # set variables
 total_votes = 0
+received_votes = []
+candidate = []
+kahn_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
     for row in csvreader:
+        
+        # count total number of votes
         total_votes = total_votes + 1
-
+        
+        # create iterable list for individual candidate's vote count
+        candidate.append(row[2])
+    
+        # store number of votes for each candidate in respctive variable
+        for i in candidate:
+            if candidate[i] == "Khan":
+                kahn_votes = kahn_votes + 1
+            elif candidate[i] == "Correy":
+                correy_votes = correy_votes + 1
+            elif candidate[i] == "Li":
+                li_votes = li_votes + 1
+            else:
+                otooley_votes = otooley_votes + 1
+        
 
 # Printed Analysis
 print("Election Results")
