ee6100010343a40c9a3c5fa3ea132dc4e4681fc8
Scott Ricketts <SJR@iMac.local>
2020-06-26 10:38:28 -0500

    added for loop

--- pybank.py/main.py
+++ pybank.py/main.py
@@ -1,24 +1,57 @@
+# import modules
 import os
 import csv
 
+# paths for csv file
 csvpath = os.path.join("Resources", "budget_data.csv")
+
+# set variables
+total_months = 0
+net_value = 0
+increase = 0
+decrease = 0
+
+
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    # print(csvreader)
-
     csv_header = next(csvreader)
-    print(csv_header)
 
+    # iterate through rows of csv file
+    for row in csv_header:
+        # total number of months
+        total_months = total_months + 1
+        
+        # total net amount
+        # net_value = net_value + int(row[1])
 
-
-
-
-
-# total_months =
-# use row[]
-# total number of months
-    # .count() -1
-# variable for first month
+        # find greatest increase
+        if int(row[1]) >= increase:
+            increase = int(row[1])
+            date_increase = str(row[0])
+        
+        # find greatest decrease
+        if int(row[1]) <= decrease:
+            decrease = int(row[1])
+            date_decrease = str(row[1])
+    
+    
+   
+    # # variable for first month
     # row [1]
-# variable for last month
+    # variable for last month
     # row [8x]
+
+
+# Printed Analysis
+print("Financial Analysis")
+print("--------------------------")
+print(f"Total Months: {total_months}")
+# how do I include dollar signs?
+print(f"Total: {net_value}")
+print(f"Average Change: ")
+print(f"Greatest Increase in Profits: {date_increase} ({increase})")
+print(f"Greatest Decrease in Profits: {date_decrease} ({decrease})")
+
+
+
+# Export text file
